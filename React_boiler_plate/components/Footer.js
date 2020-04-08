4519765d96f3de3215dfdaed62320b48e9e22b2f
yogeshdatir <yogdat@gmail.com>
2020-04-08 11:31:47 +0530

    Basic React App Boiler Plate
    Use: Python_Repo/Django React Redux/1. Django React Integration.md
    
    Reference: scrimba -
    Learn React for free / React Parent/Child Component Practice

---
+++ React_boiler_plate/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>
+            <h3>This is my footer element</h3>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ React_boiler_plate/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <main>This is the main section</main>
+    )
+}
+
+export default MainContent
